63776f079d619a2de8034aeaf50ca3cc8c671b57
tooljose88 <tooljose88@yahoo.es>
2023-06-07 00:37:34 +0200

    Added some fixes in derogation_type and Heating_mode enum

--- somfy_connected_thermostat/models.py
+++ somfy_connected_thermostat/models.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from time import time
 
 class Principal:
@@ -18,6 +19,13 @@ class Smartphone:
     self.vendor_id = vendor_id
     self.push_token = push_token
 
+class Heating_mode(Enum):
+    MANUAL = 'manuel'
+    FREEZE = 'freeze'
+    AWAY = 'away'
+    AT_HOME = 'at_home'
+    SLEEP = 'sleep'
+
 class ThermostatInfo:
   def __init__(self, temperature: float, temperature_consigne: float, battery: int, mode: str):
     self.temperature = temperature
@@ -26,58 +34,65 @@ class ThermostatInfo:
     self.mode = mode
 
 class ThermostatCommand:
-  def __init__(self, heating_mode: str, derogation_type: str, duration: int, started_at: int, finish_at: int, target_temperature: float):
-    self.heating_mode = heating_mode
+  def __init__(self, heating_mode: Heating_mode, derogation_type: str, duration: int,finish_at: int, target_temperature: float):
+    self.heating_mode = heating_mode if target_temperature > 14 else Heating_mode.FREEZE
     self.derogation_type = derogation_type
     self.duration = duration
-    self.started_at = started_at
+    self.started_at = round(time())
     self.finish_at = finish_at
     self.target_temperature = target_temperature
 
 class SetTemperatureCommand:
-  def __init__(self, target_temperature: float, derogation_type: str = 'next_mode'):
+  def __init__(self, target_temperature: float, derogation_type: str = 'further_notice', heating_mode = Heating_mode.MANUAL):
     ThermostatCommand.__init__(
       self,
-      heating_mode= 'manuel',
+      heating_mode= heating_mode,
       derogation_type= derogation_type,
-      duration= None,
-      started_at= None,
-      finish_at= None,
+      duration= -1,
+      finish_at= -1,
       target_temperature= target_temperature,
     )
 
 class SetTemperatureWithDurationCommand:
-  def __init__(self, target_temperature: float, duration: int):
+  def __init__(self, target_temperature: float, duration: int, heating_mode = Heating_mode.MANUAL):
     ThermostatCommand.__init__(
       self,
-      heating_mode= 'manuel',
-      derogation_type= 'none',
+      heating_mode= heating_mode,
+      derogation_type= 'date',
       duration= duration,
-      started_at= time(),
       finish_at= time() + duration,
       target_temperature= target_temperature,
     )
 
 class ResetDerogationCommand:
-  def __init__(self):
+  def __init__(self, target_temperature: float):
     ThermostatCommand.__init__(
       self,
       heating_mode= 'at_home',
-      derogation_type= 'none',
-      duration= 0,
-      started_at= 0,
-      finish_at= 0,
-      target_temperature= 0,
+      derogation_type= 'further_notice',
+      duration= -1,
+      finish_at= -1,
+      target_temperature= target_temperature,
     )
 
 class SetHeatingModeCommand:
-  def __init__(self, heating_mode: str):
+  def __init__(self, heating_mode: str, target_temperature: float):
     ThermostatCommand.__init__(
       self,
       heating_mode= heating_mode,
-      derogation_type= 'none',
-      duration= 0,
-      started_at= None,
-      finish_at= None,
-      target_temperature= None,
+      derogation_type= 'further_notice',
+      duration= -1,
+      finish_at= -1,
+      target_temperature= target_temperature,
+    )
+
+class SetFreezeModeCommand:
+  def __init__(self):
+    ThermostatCommand.__init__(
+      self,
+      heating_mode= 'freeze',
+      derogation_type= 'further_notice',
+      duration= -1,
+      finish_at= -1,
+      target_temperature= 8.0,
     )
